7335a981c697d49ee8edd5ebe4a9ac6d98a10298
Dauka0 <dauka0300@gmail.comm>
2022-04-14 03:38:49 +0600

    feat: created game

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-const TelegramApi = require('node-telegram-bot-api')
+const TelegramApi = require('node-telegram-bot-api');
+const {gameOptions,againOptions} = require('./options');
+const UserModel = require('./models');
+
 const token = '5263539321:AAHEgk-DAjMBfv5VfcHl4tV4l_YquiqoTRU'
 
 const bot = new TelegramApi(token, {polling: true})
 
-bot.setMyCommands([
+const chats = {}
+
+
+const start = async () => {
+    try{
+        await sequelize.authenticate()
+        await sequelize.sync()
+    }
+    catch(e){
+        console.log('Database broke')
+    }
+    bot.setMyCommands([
     {command: '/start', description: 'To start the bot'},
     {command: '/info', description: 'To see information'},
+    {command: '/game', description: 'Game, guess the number'},
+
 ])
+}
+
+const startGame = async (chatId) => {
+    await bot.sendMessage(chatId, "You have to guess one number from 0 to 9");
+    const randomNumber = Math.floor(Math.random()*10)
+    chats[chatId] = randomNumber
+    await bot.sendMessage(chatId, "Give back", gameOptions)
+}
 
 bot.on('message',async msg=>{
     const text = msg.text;
@@ -19,5 +43,22 @@ bot.on('message',async msg=>{
     if(text === '/info'){
         return bot.sendMessage(chatId, "Your first name " + msg.chat.first_name);
     }
+    if(text === '/game'){
+        return startGame(chatId)
+    }
     bot.sendMessage(chatId, "I didn't understand, try again");
 })
+bot.on('callback_query', msg=>{
+    const data = msg.data;
+    const chatId = msg.message.chat.id;
+    if(data === '/again'){
+        return startGame(chatId)
+    } 
+    if(data == chats[chatId]){
+        return bot.sendMessage(chatId, "Your answer is correct "+ chats[chatId], againOptions)
+    }   
+    else{
+        return bot.sendMessage(chatId, "Your answer is not correct, correct answer is "+ chats[chatId], againOptions)
+    }
+})
+start()
